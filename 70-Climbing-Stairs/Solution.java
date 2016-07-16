52567d219ef046a648f25afda2c78381c10709b8
Wei Shi <ws1196@nyu.edu>
2016-07-16 18:22:49 -0400

    [70. Climbing Stairs][Accepted]

--- 70-Climbing-Stairs/Solution.java
+++ 70-Climbing-Stairs/Solution.java
@@ -8,9 +8,11 @@ public class Solution {
             return 2;
         }
         int[] memo = new int[n];
+        memo[0] = 1;
+        memo[1] = 2;
         for(int i = 2; i < n; i++) {
             memo[i] = memo[i - 1] + memo[i - 2];
         }
-        return memo[n];
+        return memo[n - 1];
     }
 }
