0405ed9b144d85b50d37fb3a27bc560afffbe15d
Wei Shi <ws1196@nyu.edu>
2016-07-18 17:33:10 -0400

    [287. Find the Duplicate Number][Accepted]: brute force

---
+++ 287-Find-the-Duplicate-Number/Solution.java
@@ -0,0 +1,13 @@
+// brute-force
+public class Solution {
+    public int findDuplicate(int[] nums) {
+        for(int i = 0; i < nums.length - 1; i++) {
+            for(int j = i + 1; j < nums.length; j ++) {
+                if(nums[i] == nums[j]) {
+                    return nums[i];
+                }
+            }
+        }
+        return -1;
+    }
+}
