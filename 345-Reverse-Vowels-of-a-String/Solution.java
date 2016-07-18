990598eb30306bdd4e627bd15d73114bb53d67b3
Wei Shi <ws1196@nyu.edu>
2016-07-18 15:31:19 -0400

    [345. Reverse Vowels of a String][Accepted]

--- 345-Reverse-Vowels-of-a-String/Solution.java
+++ 345-Reverse-Vowels-of-a-String/Solution.java
@@ -1,13 +1,13 @@
 public class Solution {
     public String reverseVowels(String s) {
-        String vowel = "aeiouAEIOU";
+        String vowel = "aeiou";
         StringBuilder sb = new StringBuilder();
         int j = s.length() - 1;
         for(int i = 0; i < s.length(); i++) {
-            if(vowel.indexOf(s.charAt(i)) == -1) { // if this char is not a vowel, we append this char to sb
+            if(vowel.indexOf(Character.toLowerCase(s.charAt(i))) == -1) { // if this char is not a vowel, we append this char to sb
                 sb.append(s.charAt(i)); 
             } else { // if this char is a vowel, we append 
-                while(j >= 0 && vowel.indexOf(s.charAt(j)) == -1) {
+                while(j >= 0 && vowel.indexOf(Character.toLowerCase(s.charAt(j))) == -1) {
                     j--;
                 }
                 sb.append(s.charAt(j--));
