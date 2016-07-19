5d39f3de179700a42e0217b46a346b68c450d0cb
Wei Shi <ws1196@nyu.edu>
2016-07-19 00:05:57 -0400

    [345. Reverse Vowels of a String][Accepted]

--- 345-Reverse-Vowels-of-a-String/Solution2.java
+++ 345-Reverse-Vowels-of-a-String/Solution2.java
@@ -1,26 +1,16 @@
 // with memo
 public class Solution {
     public String reverseVowels(String s) {
-        Set<Character> vowel = new HashSet<>();
-        vowel.add('a');
-        vowel.add('e');
-        vowel.add('i');
-        vowel.add('o');
-        vowel.add('u');
-        vowel.add('A');
-        vowel.add('E');
-        vowel.add('I');
-        vowel.add('O');
-        vowel.add('U');        
+        String vowel = "aeiouAEIOU";       
         List<Character> list = new LinkedList<>();
         for(int i = 0; i < s.length(); i++) {
-            if(vowel.contains(s.charAt(i))) {
+            if(vowel.indexOf(s.charAt(i)) != -1) {
                 list.add(0, s.charAt(i));
             }
         }
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i < s.length(); i++) {
-            if(!vowel.contains(s.charAt(i))) {
+            if(vowel.indexOf(s.charAt(i)) == -1) {
                 sb.append(s.charAt(i));
             } else {
                 sb.append(list.remove(0));
