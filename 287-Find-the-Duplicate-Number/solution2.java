34df445efc10e82d44406730868f2f147ae99b98
Wei Shi <ws1196@nyu.edu>
2016-07-19 16:04:40 -0400

    [287. Find the Duplicate Number][Accepted]: slow-fast pointer

--- 287-Find-the-Duplicate-Number/solution2.java
+++ 287-Find-the-Duplicate-Number/solution2.java
@@ -1,10 +1,10 @@
 public class Solution {
     public int findDuplicate(int[] nums) {
-        int slow = 0, fast = 1;
-        while(slow != fast) {
+        int slow = 0, fast = 0;
+        do {
             slow = nums[slow];
-            fast = nums[nums[fast]];
-        }
+            fast = nums[nums[fast]];            
+        }while(slow != fast);
         int find = 0;
         while(find != slow) {
             slow = nums[slow];
