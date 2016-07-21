b97652d504ceb218b7da51dcd55b49804127d4e0
Wei Shi <ws1196@nyu.edu>
2016-07-21 16:18:28 -0400

    [336. Palindrome Pairs][Time Limit Exceeded]

---
+++ 336-Palindrome-Pairs/solution.java
@@ -0,0 +1,33 @@
+public class Solution {
+    public List<List<Integer>> palindromePairs(String[] words) {
+        List<List<Integer>> res = new ArrayList<>();
+        for(int i = 0; i < words.length - 1; i++) {
+            for(int j = i + 1; j < words.length; j++) {
+                
+                //i to j
+                if(isPalin(words[i].concat(words[j]))) {
+                    List<Integer> tmp1 =  new ArrayList<>();
+                    tmp1.add(i);
+                    tmp1.add(j);
+                    res.add(tmp1);
+                }
+                if(isPalin(words[j].concat(words[i]))) {
+                    List<Integer> tmp2 =  new ArrayList<>();
+                    tmp2.add(j);
+                    tmp2.add(i);
+                    res.add(tmp2);
+                }                
+            }
+        }
+        return res;
+    }
+    
+    private boolean isPalin(String s) {
+        for(int i = 0, j = s.length() - 1; i < j; i++, j--) {
+            if(s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
