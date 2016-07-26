1f5782c3631c84ed76edbe0446a9d21626109665
Wei Shi <ws1196@nyu.edu>
2016-07-26 13:50:20 -0400

    [8. String to Integer (atoi)][Runtime Error]

---
+++ 8-String-to-Integer-(atoi)/solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int myAtoi(String str) {
+        int sign = (str.charAt(0) == '-') ? -1 : 1;
+        int res = 0, i = sign == -1 ? 1 : 0;
+        for (; i < str.length(); i++) {
+            if (isNum(str.charAt(i))) { // this digit is a num
+                res *= 10;
+                res += (str.charAt(i) - '0');
+            } else {
+                return sign * res;
+            }
+        }
+        return sign * res;
+    }
+    
+    private boolean isNum(char ch) {
+        if(ch >= 48 && ch <= 57) {
+            return true;
+        }
+        return false;
+    }
+}
