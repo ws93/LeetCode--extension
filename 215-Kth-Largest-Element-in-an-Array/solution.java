f5ca37d50da0b8d9b8e120a0db52034140fc0922
Wei Shi <ws1196@nyu.edu>
2016-07-27 20:14:40 -0400

    [215. Kth Largest Element in an Array][Accepted]: use a priority queue

--- 215-Kth-Largest-Element-in-an-Array/solution.java
+++ 215-Kth-Largest-Element-in-an-Array/solution.java
@@ -1,7 +1,12 @@
-import java.util.*;
 public class Solution {
     public int findKthLargest(int[] nums, int k) {
-        Arrays.sort(nums);
-        return nums[nums.length - k];
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int n : nums) {
+            pq.offer(n);
+        }
+        while(pq.size() > k) {
+            pq.poll();
+        }
+        return pq.peek();
     }
 }
