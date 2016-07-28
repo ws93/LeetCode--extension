dafc6836d1246241a45d18e4849fbfbf8d767a44
Wei Shi <ws1196@nyu.edu>
2016-07-28 16:11:33 -0400

    [11. Container With Most Water][Accepted]

--- 11-Container-With-Most-Water/test.java
+++ 11-Container-With-Most-Water/test.java
@@ -1,18 +1,20 @@
-public int maxArea(int[] height) {
-    if(height == null || height.length < 2) return 0;
-    int left = 0;
-    int right = height.length-1;
-    int ans = Integer.MIN_VALUE;
-    while(left < right){
-        ans = Math.max(ans, Math.min(height[left], height[right])*(right-left));
-        if(height[left] < height[right]){
-            left++;
-        }else if(height[left] == height[right]){
-            right--;
-            left++;
-        }else{
-            right--;
+public class Solution {
+    public int maxArea(int[] height) {
+        if(height == null || height.length < 2) return 0;
+        int left = 0;
+        int right = height.length-1;
+        int ans = Integer.MIN_VALUE;
+        while(left < right){
+            ans = Math.max(ans, Math.min(height[left], height[right])*(right-left));
+            if(height[left] < height[right]){
+                left++;
+            }else if(height[left] == height[right]){
+                right--;
+                left++;
+            }else{
+                right--;
+            }
         }
+        return ans;
     }
-    return ans;
 }
