836438eb83fc3b542cee4df13f816ec8ff101123
Wei Shi <ws1196@nyu.edu>
2016-07-29 15:47:52 -0400

    [214. Shortest Palindrome][Accepted]

--- 214-Shortest-Palindrome/solution.java
+++ 214-Shortest-Palindrome/solution.java
@@ -1,36 +1,26 @@
 public class Solution {
     public String shortestPalindrome(String s) {
-        if(s == null | s.length() < 2) {
-            return s;
+        int end = s.length() - 1;
+        int j = 0;
+        while (j <= end) {
+            end = j == 0 ? end: j - 1;
+            j = 0;
+            for (int i = end; i >= 0; i--)
+                if (s.charAt(i) == s.charAt(j)) j++;
         }
-        int pos = 0;
-        //StringBuilder res = new StringBuilder();
-        for(int i = s.length() - 1; i >= 0; i--) {
-            if(isPalin(s, i)) {
-                pos = i;
-                if(i == s.length() - 1) {
-                    return s;
-                }
-                // res.append(new StringBuilder(s.substring(i + 1)).reverse().toString());
-                // res.append(s);
-                // return res.toString();
-                break;
-            }
-        }
-        return new StringBuilder(s.substring(pos + 1)).reverse().toString() + s;
-        // return res.toString();
+        return new StringBuilder(s.substring(j)).reverse().toString() + s;
     }
     
-    private boolean isPalin(String s, int right) { // determine if the substring from 0 to right is a palin
-        int left = 0;
-        while(left <= right) {
-            if(s.charAt(left) == s.charAt(right)) {
-                left++;
-                right--;
-            } else {
-                return false;
-            }
-        }
-        return true;
-    }
+    // private boolean isPalin(String s, int right) { // determine if the substring from 0 to right is a palin
+    //     int left = 0;
+    //     while(left <= right) {
+    //         if(s.charAt(left) == s.charAt(right)) {
+    //             left++;
+    //             right--;
+    //         } else {
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
 }
