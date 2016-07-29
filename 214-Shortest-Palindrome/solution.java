9fb6b21f4c9740dbd7265204175de6f0797368ac
Wei Shi <ws1196@nyu.edu>
2016-07-29 14:29:46 -0400

    [214. Shortest Palindrome][Time Limit Exceeded]

--- 214-Shortest-Palindrome/solution.java
+++ 214-Shortest-Palindrome/solution.java
@@ -3,18 +3,22 @@ public class Solution {
         if(s == null | s.length() < 2) {
             return s;
         }
-        StringBuilder res = new StringBuilder();
+        int pos = 0;
+        //StringBuilder res = new StringBuilder();
         for(int i = s.length() - 1; i >= 0; i--) {
             if(isPalin(s, i)) {
+                pos = i;
                 if(i == s.length() - 1) {
                     return s;
                 }
-                res.append(new StringBuilder(s.substring(i + 1)).reverse().toString());
-                res.append(s);
-                return res.toString();
+                // res.append(new StringBuilder(s.substring(i + 1)).reverse().toString());
+                // res.append(s);
+                // return res.toString();
+                break;
             }
         }
-        return res.toString();
+        return new StringBuilder(s.substring(pos + 1)).reverse().toString() + s;
+        // return res.toString();
     }
     
     private boolean isPalin(String s, int right) { // determine if the substring from 0 to right is a palin
