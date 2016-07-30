f4cc5fba5a93e7aaa63cc1d58e8b94f5663248fb
Wei Shi <ws1196@nyu.edu>
2016-07-30 15:04:20 -0400

    [53. Maximum Subarray][Accepted]: kadane's algo

--- 53-Maximum-Subarray/solution.java
+++ 53-Maximum-Subarray/solution.java
@@ -1,29 +1,10 @@
-// brute-force
 public class Solution {
     public int maxSubArray(int[] nums) {
-        if(nums == null || nums.length == 0) {
-            return Integer.MIN_VALUE;
+        int maxSoFar = nums[0], maxEndHere = nums[0];
+        for(int i = 1; i < nums.length; i++) {
+            maxEndHere = Math.max(maxEndHere + nums[i], nums[i]);
+            maxSoFar = Math.max(maxSoFar, maxEndHere);
         }
-        int res = nums[0];
-        for(int i = 0; i < nums.length; i++) {
-            // if(nums[i] <= 0) {
-            //     continue;
-            // }
-            for(int j = i; j < nums.length; j++) {
-                // if(nums[j] <= 0) {
-                //     continue;
-                // }
-                res = Math.max(res, countSum(nums, i, j));
-            }
-        }
-        return res;
-    }
-    
-    private int countSum(int[] nums, int left, int right) {
-        int res = 0;
-        for(int i = left; i <= right; i++) {
-            res += nums[i];
-        }
-        return res;
+        return maxSoFar;
     }
 }
