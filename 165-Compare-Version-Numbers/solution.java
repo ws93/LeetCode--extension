0efbe4dc74e1b90008b4cd7186db8c35eae7ea05
Wei Shi <ws1196@nyu.edu>
2016-08-02 16:30:27 -0400

    [165. Compare Version Numbers][Accepted]

--- 165-Compare-Version-Numbers/solution.java
+++ 165-Compare-Version-Numbers/solution.java
@@ -1,12 +1,33 @@
 public class Solution {
     public int compareVersion(String version1, String version2) {
-        double a = Double.parseDouble(version1), b = Double.parseDouble(version2);
-        if(a == b) {
-            return 0;
-        } else if(a > b) {
-            return 1;
-        } else {
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
+        int len = Math.max(v1.length, v2.length);
+        for(int i = 0; i < len; i++) {
+            String para1 = i >= v1.length ? "0" : v1[i];
+            String para2 = i >= v2.length ? "0" : v2[i];
+            int tmp = compare(para1, para2);
+            if(tmp != 0) {
+                return tmp;
+            }
+        }
+        return 0;
+    }
+    
+    private int compare(String s1, String s2) {
+        if(s1 == null) {
             return -1;
+        } else if(s2 == null) {
+            return 1;
+        }
+        int v1 = Integer.parseInt(s1);
+        int v2 = Integer.parseInt(s2);
+        if(v1 > v2) {
+            return 1;
+        } else if(v1 < v2) {
+            return -1;
+        } else {
+            return 0;
         }
     }
 }
