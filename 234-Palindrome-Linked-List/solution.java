fb7dd8c1f4e8c19faa7d92ca08e0f2717dc68177
Wei Shi <ws1196@nyu.edu>
2016-08-16 14:41:34 -0400

    [234. Palindrome Linked List][Accepted] : use a list to store value

--- 234-Palindrome-Linked-List/solution.java
+++ 234-Palindrome-Linked-List/solution.java
@@ -8,44 +8,21 @@
  */
 public class Solution {
     public boolean isPalindrome(ListNode head) {
-        if (head == null || head.next == null) {
-            return true;
+        List<Integer> list = new ArrayList<>();
+        ListNode it = head;
+        while (it != null) {
+            list.add(it.val);
+            it = it.next;
         }
-        ListNode mid = findMid(head);
-        ListNode rightHalf = reverseList(mid.next);
-        mid.next = null;
-        ListNode it1 = head;
-        ListNode it2 = rightHalf;
-        while (it2 != null) {
-            if (it1.val != it2.val) {
+        return palin(list);
+    }
+    
+    private boolean palin(List<Integer> list) {
+        for (int i = 0, j = list.size() - 1; i <= j; i++, j--) {
+            if (!list.get(i).equals(list.get(j))) {
                 return false;
             }
-            it1 = it1.next;
-            it2 = it2.next;
         }
         return true;
     }
-    
-    private ListNode findMid(ListNode head) {
-        ListNode fast = head.next;
-        ListNode slow = head;
-        while (fast != null && fast.next != null) {
-            fast = fast.next.next;
-            slow = slow.next;
-        }
-        return slow;
-    }
-    
-    private ListNode reverseList(ListNode head) {
-        ListNode prev = null;
-        ListNode curt = head;
-        while (curt != null) {
-            ListNode tmp = curt.next;
-            curt.next = prev;
-            prev = curt;
-            curt = tmp;
-        }
-        return prev;
-    }
-    
 }
