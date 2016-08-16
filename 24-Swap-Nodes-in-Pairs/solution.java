8b09b15fdaf33d7c60b66638ec7e1f755d439c47
Wei Shi <ws1196@nyu.edu>
2016-08-16 16:19:56 -0400

    [24. Swap Nodes in Pairs][Accepted]

---
+++ 24-Swap-Nodes-in-Pairs/solution.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode node = new ListNode(0);
+        node.next = head;
+        head = node;
+        
+        while (head != null && head.next != null && head.next.next != null) {
+            ListNode n1 = head.next;
+            ListNode n2 = head.next.next;
+            head.next = n2;
+            n1.next = n2.next;
+            n2.next = n1;
+            head = n1;
+        }
+        return node.next;
+    }
+}
