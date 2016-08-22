b47f491d4e7356f52ea8ca15aabd82e066b77b13
Wei Shi <ws1196@nyu.edu>
2016-08-22 14:27:01 -0400

    [23. Merge k Sorted Lists][Accepted]

---
+++ 23-Merge-k-Sorted-Lists/solution.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        for (ListNode list : lists) {
+            ListNode tmp = list;
+            while (tmp != null) {
+                pq.add(tmp.val);
+                tmp = tmp.next;
+            }
+        }
+        ListNode res = null;
+        if (lists == null || lists.length == 0) {
+            return res;
+        } 
+        res = new ListNode(0); 
+        ListNode node = res;
+        while (!pq.isEmpty()) {
+            node.next = new ListNode(pq.poll());
+            // node.val = pq.poll();
+            // if (pq.size() == 0) {
+            //     break;
+            // }
+            node = node.next;
+        }
+        return res.next;
+    }
+}
