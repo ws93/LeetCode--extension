cbb6eac39cdb7765b36697b97aa1a20a81634c59
Wei Shi <ws1196@nyu.edu>
2016-08-25 13:44:58 -0400

    [102. Binary Tree Level Order Traversal][Accepted] : correct one

--- 102-Binary-Tree-Level-Order-Traversal/solution.java
+++ 102-Binary-Tree-Level-Order-Traversal/solution.java
@@ -7,7 +7,6 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
- // this is a test
 public class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
@@ -15,19 +14,19 @@ public class Solution {
         Queue<TreeNode> queue = new LinkedList<TreeNode>();
         queue.offer(root);
         while (!queue.isEmpty()) {
-        	int size = queue.size();
-        	List<Integer> level = new ArrayList<Integer>();
-        	for (int i = 0; i < size; i++) {
-        		TreeNode head = queue.poll();
-        		level.add(head.val);
-        		if (head.left != null) {
-        			queue.offer(head.left);
-        		}
-        		if (head.left != null) {
-        			queue.offer(head.right);
-        		}        		
-        	}
-        	result.add(level);
+            int size = queue.size();
+            List<Integer> level = new ArrayList<Integer>();
+            for (int i = 0; i < size; i++) {
+                TreeNode head = queue.poll();
+                level.add(head.val);
+                if(head.left != null) {
+                    queue.offer(head.left);
+                }
+                if(head.right != null) {
+                    queue.offer(head.right);
+                }
+            }
+            result.add(level);
         }
         return result;
     }
