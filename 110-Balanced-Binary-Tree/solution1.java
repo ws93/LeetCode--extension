d5a65927d68cf93138f771580dbb9e178c934318
Wei Shi <ws1196@nyu.edu>
2016-08-31 15:23:52 -0400

    [110. Balanced Binary Tree][Accepted]: without returnType

---
+++ 110-Balanced-Binary-Tree/solution1.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return height(root) != -1;
+    }
+
+    private int height(TreeNode root) {
+    	if (root == null) {
+    		return 0;
+    	}
+    	int left = height(root.left), right = height(root.right);
+    	if (left == -1 || right == -1 || Math.abs(left - right) > 1) {
+    		return -1;
+    	}
+    	return Math.max(left, right) + 1;
+
+    }
+}
