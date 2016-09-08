a62a208a47c3b595c42c03efcf071f6abf777912
Wei Shi <ws1196@nyu.edu>
2016-09-08 18:58:44 -0400

    [55. Jump Game][Accepted]

---
+++ 55-Jump-Game/solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public boolean canJump(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return true;
+        }
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (i > max) {
+                return false;
+            }
+            max = Math.max(max, i + nums[i]);
+        }
+        return true;
+    }
+}
