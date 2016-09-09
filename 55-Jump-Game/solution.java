f67d5532da543f5d13b6416bc65dd1288964bef8
Wei Shi <ws1196@nyu.edu>
2016-09-09 13:29:21 -0400

    [55. Jump Game][Time Limit Exceeded]

--- 55-Jump-Game/solution.java
+++ 55-Jump-Game/solution.java
@@ -1,15 +1,18 @@
 public class Solution {
     public boolean canJump(int[] nums) {
         if (nums == null || nums.length == 0) {
-            return true;
+        	return true;
         }
-        int max = nums[0];
+        boolean[] dp = new boolean[nums.length];
+        dp[0] = true;
         for (int i = 1; i < nums.length; i++) {
-            if (i > max) {
-                return false;
-            }
-            max = Math.max(max, i + nums[i]);
+        	for (int j = 0; j < i; j++) {
+        		if (dp[j] && j + nums[j] >= i) {
+        			dp[i] = true;
+        			break;
+        		}
+        	}
         }
-        return true;
+        return dp[nums.length - 1];
     }
 }
