64f0c6104f4d6fdc8a5fdeb8fd6bbcd2c3108daf
Wei Shi <ws1196@nyu.edu>
2016-09-21 15:49:50 -0400

    [220. Contains Duplicate III][Time Limit Exceeded]: brute-force

--- 220-Contains-Duplicate-III/solution.java
+++ 220-Contains-Duplicate-III/solution.java
@@ -1,8 +1,8 @@
 public class Solution {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
         for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (Math.abs((long)nums[i] - (long)nums[j]) <= t && Math.abs(i - j) <= k) {
+            for (int j = i + 1; j < nums.length && j <= i + k; j++) {
+                if (Math.abs((long)nums[i] - (long)nums[j]) <= t) {
                     return true;
                 }
             }
