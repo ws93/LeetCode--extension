b3e9a139df5c1102b8d371b84051115a75fb11a0
Wei Shi <ws1196@nyu.edu>
2016-09-22 12:37:43 -0400

    [220. Contains Duplicate III][Time Limit Exceeded]: use TreeSet

--- 220-Contains-Duplicate-III/solution.java
+++ 220-Contains-Duplicate-III/solution.java
@@ -1,10 +1,15 @@
 public class Solution {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length && j <= i + k; j++) {
-                if (Math.abs((long)nums[i] - (long)nums[j]) <= t) {
-                    return true;
-                }
+        TreeSet<Integer> set = new TreeSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            Integer floor = set.floor(nums[i] + t);
+            Integer ceiling = set.ceiling(nums[i] - t);
+            if (floor != null && floor >= nums[i] || ceiling != null && ceiling <= nums[i]) {
+                return true;
+            }
+            set.add(nums[i]);
+            if (i >= k) {
+                set.remove(nums[i - k]);
             }
         }
         return false;
