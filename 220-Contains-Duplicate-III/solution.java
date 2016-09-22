b719e26873c46638e73b41bee98e118d5bfc7f9c
Wei Shi <ws1196@nyu.edu>
2016-09-22 14:50:14 -0400

    [220. Contains Duplicate III][Time Limit Exceeded]: use Bucket O(N)

--- 220-Contains-Duplicate-III/solution.java
+++ 220-Contains-Duplicate-III/solution.java
@@ -1,16 +1,21 @@
 public class Solution {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-        TreeSet<Integer> set = new TreeSet<>();
+        if (k == 0 || t < 0) {
+            return false;
+        }
+        Map<Long, Long> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            Integer floor = set.floor(nums[i] + t);
-            Integer ceiling = set.ceiling(nums[i] - t);
-            if (floor != null && floor >= nums[i] || ceiling != null && ceiling <= nums[i]) {
-                return true;
+            long newNum = (long)nums[i] - Integer.MIN_VALUE;
+            long bucket = newNum / ((long)t + 1);
+            if (map.containsKey(bucket) 
+                || (map.containsKey(bucket + 1) && map.get(bucket + 1) - newNum <= t)
+                || (map.containsKey(bucket - 1) && newNum - map.get(bucket - 1) <= t)) {
+                    return true;
             }
-            set.add(nums[i]);
-            if (i >= k) {
-                set.remove(nums[i - k]);
+            if (map.entrySet().size() >= k) {
+                map.remove(((long)nums[i - k] - Integer.MIN_VALUE) / ((long)t + 1));
             }
+            map.put(bucket, newNum);
         }
         return false;
     }
