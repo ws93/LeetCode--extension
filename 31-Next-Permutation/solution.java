ebb52dd3fafe8277debc40dfcf517318aa992c27
Wei Shi <ws1196@nyu.edu>
2016-10-02 14:53:08 -0400

    [31. Next Permutation][Accepted]

---
+++ 31-Next-Permutation/solution.java
@@ -0,0 +1,27 @@
+public class Solution {
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        int j = nums.length - 1;
+        if (i >= 0) {
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);            
+        }
+        reverse(nums, i + 1, nums.length - 1);
+    }
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+    private void reverse(int[] nums, int i, int j) {
+        int left = i, right = j;
+        while (left < right) {
+            swap(nums, left++, right--);
+        }
+    }
+}
