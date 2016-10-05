4434a9d412f0c286cf2f4d867cba5df8e1558bd9
Wei Shi <ws1196@nyu.edu>
2016-10-05 14:44:21 -0400

    [385. Mini Parser][Accepted]

---
+++ 385-Mini-Parser/solution.java
@@ -0,0 +1,57 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *     // Constructor initializes an empty nested list.
+ *     public NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     public NestedInteger(int value);
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     public void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     public void add(NestedInteger ni);
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return null if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+public class Solution {
+    public NestedInteger deserialize(String s) {
+        if (!s.startsWith("[")) {
+            return new NestedInteger(Integer.valueOf(s));
+        }
+        Stack<NestedInteger> stack = new Stack<>();
+        NestedInteger res = new NestedInteger();
+        stack.push(res);
+        int start = 1;
+        for (int i = 1; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '[') {
+                NestedInteger ni = new NestedInteger();
+                stack.peek().add(ni);
+                stack.push(ni);
+                start = i + 1;
+            } else if (ch == ',' || ch == ']') {
+                if (start < i) {
+                    stack.peek().add(new NestedInteger(Integer.valueOf(s.substring(start, i))));
+                }
+                start = i + 1; 
+                if (ch == ']') {
+                    stack.pop();
+                }
+            }
+        }
+        return res;
+    }
+}
