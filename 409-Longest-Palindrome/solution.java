31604a4a97727ba23e36f9fb1e392e237ffdac03
Wei Shi <ws1196@nyu.edu>
2016-10-12 19:00:54 -0400

    [409. Longest Palindrome][Accepted]

---
+++ 409-Longest-Palindrome/solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int longestPalindrome(String s) {
+        if (s == null || s.length() == 0) return 0;
+        Set<Character> set = new HashSet<>();
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (set.contains(s.charAt(i))) {
+                set.remove(s.charAt(i));
+                count++;
+            } else {
+                set.add(s.charAt(i));
+            }
+        }
+        if (!set.isEmpty()) return count * 2 + 1;
+        return count * 2;
+    }
+}
